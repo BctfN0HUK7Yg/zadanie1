4ab526126ffdd64cd58874a116ce20bb6ab2f78b
BctfN0HUK7Yg <oksimets@yandex.ru>
2022-02-19 14:20:50 +0300

    123

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,81 @@
 use std::thread;
 use std::time::Duration;
 
-const TRASHHOLD:usize=5;
+const TRASHHOLD: usize = 5;
 
-
-fn f<T,R>(_t:&T)->R 
-    where R:Default {
+fn f<T, R>(_t: &T) -> R
+where
+    R: Default,
+{
     thread::sleep(Duration::from_millis(500));
     R::default()
 }
 
-fn split_work<T,R> (work:&Vec<T>) -> Vec<R> 
-    where T:Sized,T:Sync, R:Default, R:Send, R:Clone {
+fn split_work<T, R>(work: &Vec<T>) -> Vec<R>
+where
+    T: Sized,
+    T: Sync,
+    R: Default,
+    R: Send,
+    R: Clone,
+{
+    let mut whole_res = Vec::new(); // for collect results
 
-     
-    let mut whole_res=Vec::new(); // for collect results
+    let mut ranges_of_work = Vec::new();
+    for i in (0..work.len()).step_by(TRASHHOLD) {
+        let size_of_work = if i + TRASHHOLD > work.len() {
+            work.len() - i
+        } else {
+            TRASHHOLD
+        }; //last part should be less then TRASHHOLD
 
-    let mut ranges_of_work=Vec::new();
-    for i in (0..work.len()).step_by(TRASHHOLD) { 
-
-        let size_of_work = if i+TRASHHOLD>work.len() {work.len()-i} else {TRASHHOLD}; //last part should be less then TRASHHOLD
-        
-        ranges_of_work.push((i,i+size_of_work));
+        ranges_of_work.push((i, i + size_of_work));
     }
-    let num_of_threasds=if work.len() % TRASHHOLD>0 {work.len() / TRASHHOLD +1} else {work.len() / TRASHHOLD};
+    let num_of_threasds = if work.len() % TRASHHOLD > 0 {
+        work.len() / TRASHHOLD + 1
+    } else {
+        work.len() / TRASHHOLD
+    };
 
-        crossbeam::scope(|s|{
-            let mut handles=Vec::new();
-            
-            for y in 0..num_of_threasds {
-                let current_range=ranges_of_work[y];
-                let part_of_work=&work[current_range.0..current_range.1]; //get slice of works
-                println!("Start thread {} to compute work range from {} to {}",y,current_range.0,current_range.1);
-            let handle =s.spawn(move |_| {
-                let mut res:Vec<R>=Vec::new();
-                
-                 for i in 0..part_of_work.len() {
-                    println!("\tdoing work in thread {:?} with element {}",y,y*(current_range.1-current_range.0)+ i);
+    crossbeam::scope(|s| {
+        let mut handles = Vec::new();
+
+        for y in 0..num_of_threasds {
+            let current_range = ranges_of_work[y];
+            let part_of_work = &work[current_range.0..current_range.1]; //get slice of works
+            println!(
+                "Start thread {} to compute work range from {} to {}",
+                y, current_range.0, current_range.1
+            );
+            let handle = s.spawn(move |_| {
+                let mut res: Vec<R> = Vec::new();
+
+                for i in 0..part_of_work.len() {
+                    println!("\tdoing work in thread {:?} with element {}", y, i);
                     res.push(f(&part_of_work[i]));
                 }
-                
+
                 res
             });
             handles.push(handle);
-            
         }
         for handle in handles {
-            let this_res=handle.join().unwrap();
+            let this_res = handle.join().unwrap();
             whole_res.extend_from_slice(&this_res);
         }
-            
-        }).unwrap();
+    })
+    .unwrap();
 
-   
-    
     whole_res
 }
 
 fn main() {
-    let input=vec![0;30];
-    println!("Start compute {} tasks",input.len());
-    println!("Trashhold for spawn thread is {} tasks",TRASHHOLD);
-    let res=split_work::<_,i32>(&input);
-    println!("\nResult: {:?}",res);
-    println!("Result length: {:?}",res.len());
+    let input = vec![0; 33];
+    println!("Start compute {} tasks", input.len());
+    println!("Trashhold for spawn thread is {} tasks", TRASHHOLD);
+    let res = split_work::<_, i32>(&input);
+    println!("\nResult: {:?}", res);
+    println!("Result length: {:?}", res.len());
 }
 
 #[cfg(test)]
@@ -71,45 +83,45 @@ mod tests {
     //everywhere output is vec of default values
     #[test]
     fn input_30_i32_output_i32() {
-        let input=vec![1;30];
-        let res=crate::split_work::<_,i32>(&input);
-        let rigth_res=vec![0;30];
+        let input = vec![1; 30];
+        let res = crate::split_work::<_, i32>(&input);
+        let rigth_res = vec![0; 30];
         assert_eq!(res, rigth_res);
     }
     #[test]
     fn input_32_f64_output_i32() {
-        let input=vec![1.0;32];
-        let res=crate::split_work::<_,i32>(&input);
-        let rigth_res=vec![0;32];
+        let input = vec![1.0; 32];
+        let res = crate::split_work::<_, i32>(&input);
+        let rigth_res = vec![0; 32];
         assert_eq!(res, rigth_res);
     }
     #[test]
     fn input_50_f64_output_f64() {
-        let input=vec![1.0;50];
-        let res=crate::split_work::<_,f64>(&input);
-        let rigth_res=vec![0.0;50];
+        let input = vec![1.0; 50];
+        let res = crate::split_work::<_, f64>(&input);
+        let rigth_res = vec![0.0; 50];
         assert_eq!(res, rigth_res);
     }
     #[test]
     fn input_13_str_output_i32() {
-        let input=vec!["0";13];
-        let res=crate::split_work::<_,i32>(&input);
-        let rigth_res=vec![0;13];
+        let input = vec!["0"; 13];
+        let res = crate::split_work::<_, i32>(&input);
+        let rigth_res = vec![0; 13];
         assert_eq!(res, rigth_res);
     }
 }
 
-/* Implement basic function to split some generic computational work between threads. 
-Split should occur only on some threshold - if computational work (input length) is shorter than this threshold, 
+/* Implement basic function to split some generic computational work between threads.
+Split should occur only on some threshold - if computational work (input length) is shorter than this threshold,
 no splitting should occur and no threads should be created.
 
-You get as input: 
+You get as input:
 
 1. Vec<T>
 2. Function f(t: T) -> R
 
 
-Threshold can be just constant. 
+Threshold can be just constant.
 
 You should return:
    1. Up to you, but probably some Vec of the same length as input(1)
